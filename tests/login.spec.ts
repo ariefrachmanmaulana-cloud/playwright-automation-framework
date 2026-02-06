bc3288a1f55f09594ba7d10222a5647063c1abe3
testing <arief.rachman@finxpert.id>
2026-02-06 17:44:32 +0700

    update list cicd

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test('Skenario 1: Invalid Email dan Password', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  
-  // 1. Buka halaman login
-  await loginPage.goto();
+// Nama di dalam tanda kutip ini yang akan muncul sebagai Judul Test Case di Allure
+test('Skenario 01: Login dengan Email dan Password Invalid', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    
+    // 1. Buka halaman login
+    await loginPage.goto();
 
-  // 2. Ambil data dari .env
-  const email = process.env.INVALID_EMAIL || '';
-  const password = process.env.INVALID_PASSWORD || '';
+    // 2. Ambil data dari .env
+    const email = process.env.INVALID_EMAIL || '';
+    const password = process.env.INVALID_PASSWORD || '';
 
-  // 3. Eksekusi login sampai klik button
-  await loginPage.login(email, password);
-  
-  // Test selesai di sini. Jika tidak ada error saat klik, maka test dianggap PASS.
+    // 3. Eksekusi login sampai klik button
+    // Jika ingin lebih dari satu akun, tinggal duplikasi baris ini atau ganti variabelnya
+    await loginPage.login(email, password);
+    
+    // Test dianggap PASS jika tidak ada error saat eksekusi login
 });
